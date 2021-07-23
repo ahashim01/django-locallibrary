899c34a2836cc6757fdceb5c029c63d9225d9eb6
Ahmed Hashim <ahmedalaahashem01@gmail.com>
2021-07-23 16:07:12 +0200

    first commit

---
+++ locallibrary/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('catalog/', include('catalog.urls')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
